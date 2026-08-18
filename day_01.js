55b12cd6cc60866ac5135b62ec12dd5f176066e5
SRIBHASHYAM VENKATA SAI GOPAL <venkatasai2006@outlook.com>
2026-08-18 12:32:07 +0530

    Startd Promises

--- day_01.js
+++ day_01.js
@@ -3,3 +3,10 @@ console.log("Hello World!!")
 const myVar="math"
 
 console.log(myVar.length)
+
+let sayHello=()=>{
+    console.log("Hello JavaScript!!");
+    console.log("Im Gopal Nice to Meet you!!");
+}
+
+sayHello()
