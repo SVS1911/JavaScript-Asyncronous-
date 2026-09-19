1e9e8947f08be16008bf2cd6fb5502e2cb7b8fd1
SRIBHASHYAM VENKATA SAI GOPAL <venkatasai2006@outlook.com>
2026-09-19 12:31:06 +0530

    started

---
+++ react/svsg_01/src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+export default function Sidebar(){
+    return(
+        <ol>
+           <li>Home</li>
+           <li>Cart</li>
+           <li>Categories</li>
+           <li>Orders</li>
+           <li>Returns</li>
+        </ol>
+    );
+}
